da0f7362f0bc9bdd2defa23542a324d94bfacfef
PedroVilares <47793686+PedroVilares@users.noreply.github.com>
2020-05-20 20:19:11 +0100

    added fragments and drawer item clicks

--- app/src/main/java/com/example/happyhealthier/MainActivity.java
+++ app/src/main/java/com/example/happyhealthier/MainActivity.java
@@ -7,18 +7,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.example.happyhealthier.fragments.AboutFragment;
+import com.example.happyhealthier.fragments.ExerciseFragment;
+import com.example.happyhealthier.fragments.MeasurementsFragment;
+import com.example.happyhealthier.fragments.ProfileFragment;
+import com.example.happyhealthier.fragments.SettingsFragment;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -37,21 +46,62 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Navigation drawer
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         toggle.syncState();
 
+        //Setting text on NavDrawer to user's name
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        TextView navUsername = headerView.findViewById(R.id.nav_nameText);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String text = navUsername.getText().toString();
+        Log.e("name",text);
+        assert user != null;
+        navUsername.setText(user.getDisplayName());
 
+        //Getting different login providers (Email,Google,Facebook)
         providers = Arrays.asList(
                 new AuthUI.IdpConfig.EmailBuilder().build(),
                 new AuthUI.IdpConfig.GoogleBuilder().build(),
                 new AuthUI.IdpConfig.FacebookBuilder().build()
         );
 
+        if (savedInstanceState == null) {
+            //showSignInOptions();
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MeasurementsFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_measures);
+        }
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_measures:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MeasurementsFragment()).commit();
+                        break;
+                    case R.id.nav_exercise:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ExerciseFragment()).commit();
+                        break;
+                    case R.id.nav_profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
+                        break;
+                    case R.id.nav_settings:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
+                        break;
+                    case R.id.nav_about:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AboutFragment()).commit();
+                        break;
+                }
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
 //        mSignOutButton = findViewById(R.id.signOutButton);
-        showSignInOptions();
 //        mSignOutButton.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
@@ -103,6 +153,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
 
     @Override
     public void onBackPressed() {
