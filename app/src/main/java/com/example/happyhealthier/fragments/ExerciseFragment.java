6b2f52daa3fec7cd7db0bab8e4f98df085c7507c
PedroVilares <47793686+PedroVilares@users.noreply.github.com>
2020-05-21 18:58:24 +0100

    addes maps and chronometer

--- app/src/main/java/com/example/happyhealthier/fragments/ExerciseFragment.java
+++ app/src/main/java/com/example/happyhealthier/fragments/ExerciseFragment.java
@@ -2,13 +2,21 @@ package com.example.happyhealthier.fragments;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Chronometer;
 
 import com.example.happyhealthier.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.MapStyleOptions;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,11 +27,65 @@ public class ExerciseFragment extends Fragment {
         // Required empty public constructor
     }
 
+    MapView mMapView;
+    private GoogleMap mGoogleMap;
+    Button playButton;
+    CardView card1;
+    Chronometer chronometer;
+
+    //TODO: Sacar o icone de stop
+    //TODO: Configurar o cronometro do cardview 2
+    //TODO: Configurar os botões
+    //TODO: Botão da música
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exercise, container, false);
+        View v = inflater.inflate(R.layout.fragment_exercise, container, false);
+
+
+        mMapView = v.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.onResume();
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mGoogleMap = googleMap;
+                mGoogleMap.setMyLocationEnabled(true);
+            }
+        });
+        return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
