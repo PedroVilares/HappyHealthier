888315529ace796247b6a07899ffa6bbba7dea64
PedroVilares <47793686+PedroVilares@users.noreply.github.com>
2020-06-02 15:55:17 +0100

    switch

--- app/src/main/java/com/example/happyhealthier/main_fragments/SettingsFragment.java
+++ app/src/main/java/com/example/happyhealthier/main_fragments/SettingsFragment.java
@@ -1,19 +1,28 @@
 package com.example.happyhealthier.main_fragments;
 
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 
 import com.example.happyhealthier.R;
+
+import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SettingsFragment extends Fragment {
+
+    Switch aSwitch;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -24,6 +33,28 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View v = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        aSwitch = v.findViewById(R.id.switch1);
+        aSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (aSwitch.isChecked()){
+                    setLocale("eng");
+                } else {
+                    setLocale("pt");
+                }
+            }
+        });
+
+        return v;
+    }
+
+    private void setLocale(String idioma){
+        Resources res = getResources();
+        DisplayMetrics displayMetrics = res.getDisplayMetrics();
+        Configuration configuration = res.getConfiguration();
+        configuration.setLocale(new Locale(idioma));
+        res.updateConfiguration(configuration,displayMetrics);
     }
 }
