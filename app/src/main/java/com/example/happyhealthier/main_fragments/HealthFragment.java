502fa5c3c3b04a0131548f55f3eaf5ca437764ce
PedroVilares <47793686+PedroVilares@users.noreply.github.com>
2020-06-08 12:20:29 +0100

    health fragment

---
+++ app/src/main/java/com/example/happyhealthier/main_fragments/HealthFragment.java
@@ -0,0 +1,33 @@
+package com.example.happyhealthier.main_fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.happyhealthier.R;
+
+public class HealthFragment extends Fragment {
+
+    public HealthFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_health, container, false);
+        return v;
+    }
+}
